3931f5f8cd3a42699550f094033bebabb223f9af
Mm <MM@MacBook-Pro-de-Mm.local>
2017-07-19 04:13:30 -0700

    Completed setting up Firebase integration

---
+++ Hipsta/SignInVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Hipsta
+//
+//  Created by Mm on 16/07/17.
+//  Copyright © 2017 Mm. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class SignInVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Hipsta/Pods-Hipsta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hipsta : NSObject
+@end
+@implementation PodsDummy_Pods_Hipsta
+@end
